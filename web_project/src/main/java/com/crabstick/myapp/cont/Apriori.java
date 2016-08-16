de2e12d89dedf2992919027bc70b2eb12d892068
younjo <yjo543sa@gmail.com>
2016-08-16 16:58:47 +0900

    추천 경로 설정

---
+++ web_project/src/main/java/com/crabstick/myapp/cont/Apriori.java
@@ -0,0 +1,18 @@
+package com.crabstick.myapp.cont;
+
+import java.util.ArrayList;
+
+import com.crabstick.myapp.venue.Venue;
+
+public class Apriori {
+	
+	/* 최초 추천 관광 지역 지지도 계산 */
+	public void apriori_Algorithm(ArrayList<Integer> transactions, ArrayList<String> seqeunces, ArrayList<Venue> venues) {
+		System.out.println("들어옴");
+		System.out.println(transactions.size());
+		System.out.println(seqeunces.size());
+		System.out.println(venues.size());
+
+	}
+
+}
--- web_project/src/main/java/com/crabstick/myapp/recommendation/RecommendationMapper.java
+++ web_project/src/main/java/com/crabstick/myapp/recommendation/RecommendationMapper.java
@@ -2,6 +2,8 @@ package com.crabstick.myapp.recommendation;
 
 import java.util.ArrayList;
 
+import com.crabstick.myapp.venue.Venue;
+
 public interface RecommendationMapper {
 	
 
@@ -11,6 +13,12 @@ public interface RecommendationMapper {
 	void update_Loc_Code(City update_City);
 
 	ArrayList<City> select_All();
+
+	ArrayList<Integer> select_All_Transaction();
+
+	ArrayList<Venue> all_Data();
+
+	ArrayList<String> all_Sequence();
 	
 
 }
